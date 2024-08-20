fcf02eb178d9bd7e2c263b62f2e41fefda070164
Sunnyvale City Roleplay <103292931+sunnyvalecityrp@users.noreply.github.com>
2024-08-20 10:18:55 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,44 @@
+local properties = {
+    {id = 1, name = "Eclipse Apartments"},
+    -- Add more properties as needed
+}
 
+local ownedProperties = {}
+
+RegisterServerEvent('property:buy')
+AddEventHandler('property:buy', function(propertyId)
+    local _source = source
+    local property = getPropertyById(propertyId)
+
+    if property then
+        if not ownedProperties[propertyId] then
+            ownedProperties[propertyId] = _source
+            TriggerClientEvent('property:owned', _source, propertyId)
+            TriggerClientEvent('chat:addMessage', _source, { args = { 'You bought ' .. property.name } })
+        else
+            TriggerClientEvent('property:alreadyOwned', _source)
+        end
+    end
+end)
+
+RegisterServerEvent('property:sell')
+AddEventHandler('property:sell', function(propertyId)
+    local _source = source
+
+    if ownedProperties[propertyId] == _source then
+        ownedProperties[propertyId] = nil
+        TriggerClientEvent('property:sold', _source, propertyId)
+        TriggerClientEvent('chat:addMessage', _source, { args = { 'You sold ' .. getPropertyById(propertyId).name } })
+    else
+        TriggerClientEvent('property:notOwner', _source)
+    end
+end)
+
+function getPropertyById(id)
+    for _, property in pairs(properties) do
+        if property.id == id then
+            return property
+        end
+    end
+    return nil
+end
