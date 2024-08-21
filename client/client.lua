b4ad284220a5d9ba95989f44307a6f738a1cefde
Sunnyvale City Roleplay <103292931+sunnyvalecityrp@users.noreply.github.com>
2024-08-21 17:19:48 +0100

    Update client.lua
    When a player purchases a property, the blip now changes from ‘for sale’ to ‘home.

--- client/client.lua
+++ client/client.lua
@@ -5,7 +5,7 @@ local properties = {
         id = 1, -- Unique identifier for the property
         name = "Property 1", -- Name of the property
         coords = vector3(-778.2485, 299.2582, 85.75365), -- Coordinates for the property location
-        price = 500000, -- Price of the property
+        price = 50000, -- Price of the property
         interior = {
             ipl = "apa_v_mp_h_04_a", -- IPL (Interior Proxy Library) name for the interior
             coords = vector3(-781.9878, 323.6537, 176.8037), -- Coordinates for where it will tp player
@@ -18,6 +18,66 @@ local properties = {
     -- Add more properties as needed
 }
 
+local ownedProperties = {}
+local props = {}
+local blips = {}
+
+Citizen.CreateThread(function()
+    for _, property in pairs(properties) do
+        local blip = AddBlipForCoord(property.coords)
+        SetBlipSprite(blip, 375)
+        SetBlipDisplay(blip, 4)
+        SetBlipScale(blip, 0.8)
+        SetBlipColour(blip, 2)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(property.name)
+        EndTextCommandSetBlipName(blip)
+        blips[property.id] = blip
+    end
+end)
+
+RegisterNetEvent('property:owned')
+AddEventHandler('property:owned', function(propertyId)
+    ownedProperties[propertyId] = true
+    if props[propertyId] then
+        DeleteObject(props[propertyId])
+        props[propertyId] = nil
+    end
+    if blips[propertyId] then
+        RemoveBlip(blips[propertyId])
+        blips[propertyId] = nil
+    end
+    local houseBlip = AddBlipForCoord(properties[propertyId].coords)
+    SetBlipSprite(houseBlip, 40) -- House blip
+    SetBlipDisplay(houseBlip, 4)
+    SetBlipScale(houseBlip, 0.8)
+    SetBlipColour(houseBlip, 3)
+    SetBlipAsShortRange(houseBlip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("My Property")
+    EndTextCommandSetBlipName(houseBlip)
+    blips[propertyId] = houseBlip
+end)
+
+RegisterNetEvent('property:alreadyOwned')
+AddEventHandler('property:alreadyOwned', function()
+    -- Notify the player that the property is already owned
+end)
+
+RegisterNetEvent('property:sold')
+AddEventHandler('property:sold', function(propertyId)
+    ownedProperties[propertyId] = nil
+    if blips[propertyId] then
+        RemoveBlip(blips[propertyId])
+        blips[propertyId] = nil
+    end
+end)
+
+RegisterNetEvent('property:notOwner')
+AddEventHandler('property:notOwner', function()
+    -- Notify the player that they are not the owner
+end)
 
 Citizen.CreateThread(function()
     while true do
@@ -33,7 +93,7 @@ Citizen.CreateThread(function()
                         PlaceObjectOnGroundProperly(prop)
                         props[property.id] = prop
                     end
-                    DrawText3D(property.coords, string.format("[E] Buy Property - $%d", property.price))
+                    DrawText3D(property.coords, "[E] Buy Property")
                     if IsControlJustReleased(0, 38) then
                         TriggerServerEvent('property:buy', property.id)
                     end
@@ -41,14 +101,13 @@ Citizen.CreateThread(function()
                     DrawText3D(property.coords, "[E] Enter Property")
                     if IsControlJustReleased(0, 38) then
                         RequestIpl(property.interior.ipl)
-                        SetEntityCoords(playerPed, property.interior.coords.x, property.interior.coords.y, property.interior.coords.z)
+                        SetEntityCoords(playerPed, property.interior.coords)
                     end
                 end
             end
         end
     end
 end)
-
 
 Citizen.CreateThread(function()
     while true do
@@ -59,18 +118,12 @@ Citizen.CreateThread(function()
         for _, property in pairs(properties) do
             if ownedProperties[property.id] then
                 for _, exit in pairs(property.interior.exits) do
-                    if #(playerCoords - exit) < 10.0 then
-                        -- Draw the marker at the exit
-                        DrawMarker(1, exit.x, exit.y, exit.z - 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 255, 0, 0, 150, false, true, 2, nil, nil, false)
-
-                        if #(playerCoords - exit) < 1.5 then
-                            -- Show text prompt
-                            ShowHelpNotification("[E] Exit Property")
-
-                            if IsControlJustReleased(0, 38) then
-                                SetEntityCoords(playerPed, property.coords.x, property.coords.y, property.coords.z)
-                                RemoveIpl(property.interior.ipl)
-                            end
+                    DrawMarker(1, exit.x, exit.y, exit.z - 1.0, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 1.0, 255, 0, 0, 100, false, true, 2, nil, nil, false)
+                    if #(playerCoords - exit) < 1.5 then
+                        DrawText3D(exit, "[E] Exit Property")
+                        if IsControlJustReleased(0, 38) then
+                            SetEntityCoords(playerPed, property.coords)
+                            RemoveIpl(property.interior.ipl)
                         end
                     end
                 end
@@ -78,14 +131,6 @@ Citizen.CreateThread(function()
         end
     end
 end)
-
-
-function ShowHelpNotification(msg)
-    AddTextEntry('HelpNotification', msg)
-    BeginTextCommandDisplayHelp('HelpNotification')
-    EndTextCommandDisplayHelp(0, false, true, -1)
-end
-
 
 function DrawText3D(coords, text)
     local onScreen, _x, _y = World3dToScreen2d(coords.x, coords.y, coords.z)
@@ -98,5 +143,5 @@ function DrawText3D(coords, text)
     AddTextComponentString(text)
     DrawText(_x, _y)
     local factor = (string.len(text)) / 370
-    DrawRect(_x, _y + 0.0125, 0.015 + factor, 0, 0, 0, 75)
+    DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 0, 0, 0, 75)
 end
